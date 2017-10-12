f39db1868afdae2aef53a898869aaf25d3f8668b
Jamie Scott <github@jami.org.uk>
2017-10-12 20:22:24 +0100

    Initial code commit

---
+++ Makefile
@@ -0,0 +1,25 @@
+.DEFAULT_GOAL := run
+.PHONY: clean, depends, run, deploy, cloud-logs, view
+
+# This will need changing to wherever you installed your GCloud SDK - please don't commit changes to this line though!
+gcloud-sdk = /opt/google-cloud-sdk
+
+clean: # Clean build files
+	rm -rf *.pyc
+	rm -rf __pycache__/
+	rm -rf **/__pycache__/
+
+depends: # Install all dependancies into `lib`
+	pip install -t lib -r requirements.txt
+
+run: # Run app locally
+	/usr/bin/env python $(gcloud-sdk)/platform/google_appengine/dev_appserver.py --host 127.0.0.1 .
+
+deploy: # Deploy app to AppEngine
+	gcloud app deploy --quiet
+
+cloud-logs: # Watch AppEngine cloud logs
+	gcloud app logs tail -s default
+
+view: # Open deployed app in browser
+	gcloud app browse
---
+++ hotspots/views.py
@@ -0,0 +1,34 @@
+import flask
+from hotspots import app
+import logging
+
+@app.route('/')
+def index():
+    logging.info('The index page was requested!')
+    return flask.render_template('index.html', title='Index')
+
+## Error Handlers
+
+@app.errorhandler(400)
+def error_400(error):
+    return flask.render_template('error.html', title='400', heading='Error 400', text="Oh no, that's an error!")
+
+@app.errorhandler(401)
+def error_401(error):
+    return flask.render_template('error.html', title='401', heading='Error 401', text="Oh no, that's an error!")
+
+@app.errorhandler(403)
+def error_403(error):
+    return flask.render_template('error.html', title='403', heading='Error 403', text="Oh no, that's an error!")
+
+@app.errorhandler(404)
+def error_404(error):
+    return flask.render_template('error.html', title='404', heading='Error 404', text="Oh no, that's an error!")
+
+@app.errorhandler(405)
+def error_405(error):
+    return flask.render_template('error.html', title='405', heading='Error 405', text="Oh no, that's an error!")
+
+@app.errorhandler(500)
+def error_500(error):
+    return flask.render_template('error.html', title='500', heading='Error 500', text="Oh no, that's an error!")
