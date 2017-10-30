faaafd363986bb6a0d8b7fe79e632fb46afe2c9a
Jamie Scott <github@jami.org.uk>
2017-10-30 18:19:17 +0000

    Added app name to config and template

--- hotspots/__init__.py
+++ hotspots/__init__.py
@@ -2,5 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_json('../config.json')
+app.jinja_env.globals['APP_NAME'] = app.config['APP_NAME'] # Set global app name in Jinja2 too
 
 from hotspots import views
