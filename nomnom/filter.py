7b578ac687157f28eddb4aa7e15ef6ee103cb80d
Steffan P <stp1g15@soton.ac.uk>
2017-11-11 20:21:47 +0000

    Filter class, matches regex pattern. Will catch some variations of the words on the blocklist.

--- nomnom/filter.py
+++ nomnom/filter.py
@@ -1,13 +1,25 @@
+from __future__ import print_function
 import json
+import re
 
 
 class Filter:
-    def __init__(self, wl_path):
-        with open(wl_path) as wl_file:
-            self._wordlist = json.loads(wl_file)
+    def __init__(self, fs_path):
+        with open(fs_path) as fs_file:
+            filter_settings = json.loads(fs_file)
+        word_list = filter_settings['word_list']
+        regex_patterns = filter_settings['regex']
+        keys = sorted(regex_patterns.keys())
+        for i in range(len(word_list)):
+            word = word_list[i]
+            for key in keys:
+                word.replace(key, regex_patterns[key])
+            word_list[i] = word
+        self._word_list = word_list
 
-    def is_obscene(self, string):
-        for word in self._wordlist:
-            if ' {} '.format(word) in string:
-                return True
-        return False
+    def is_safe(self, string):
+        for word in self._word_list:
+            match = re.search(word, string)
+            if match:
+                return False
+        return True
